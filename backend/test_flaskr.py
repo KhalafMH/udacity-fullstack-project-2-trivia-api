b4f5c9996fa7e7087997364c7756726cf1756a03
Mahdi AlKhalaf <m@mahdi.pro>
2020-07-23 03:17:22 +0300

    test: Add test for the `GET` `/api/categories` endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,10 +1,9 @@
-import os
 import unittest
-import json
+
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import setup_db
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +14,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format('postgres:password@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +23,30 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
+            self.db.engine.execute("""
+                INSERT INTO categories (type) VALUES ('Science'), ('Sport')
+            """)
 
     def tearDown(self):
         """Executed after reach test"""
+        with self.app.app_context():
+            self.db.engine.execute("""
+                DELETE FROM categories
+            """)
         pass
 
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories_returns_correct_result(self):
+        client = self.client()
+        result = client.get("/api/categories")
+        self.assertEqual(200, result.status_code)
+        self.assertIn('Sport', result.json['categories'])
+        self.assertIn('Science', result.json['categories'])
+        self.assertEqual(2, len(result.json['categories']))
 
 
 # Make the tests conveniently executable
