2cc98950a02479a7e15b4a6a56e557fcfaac5ee3
Mahdi AlKhalaf <m@mahdi.pro>
2020-07-26 00:30:36 +0300

    style: Wrap long lines

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,7 +12,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('postgres:password@localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'postgres:password@localhost:5432', self.database_name)
         self.db = setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,14 +25,16 @@ class TriviaTestCase(unittest.TestCase):
             """)
             self.db.create_all()
             self.db.engine.execute("""
-                INSERT INTO categories (id, type) VALUES (1, 'Science'), (2, 'Sport'), (3, 'History');
+                INSERT INTO categories (id, type) 
+                VALUES (1, 'Science'), (2, 'Sport'), (3, 'History');
             """)
             self.db.engine.execute("""
                 INSERT INTO questions (question, answer, difficulty, category) 
                 VALUES ('What is the answer to everything', '42', 3, 1);
 
                 INSERT INTO questions (question, answer, difficulty, category) 
-                VALUES ('When did the big bang happen', 'When the universe was created', 3, 1);
+                VALUES ('When did the big bang happen', 
+                        'When the universe was created', 3, 1);
                 
                 INSERT INTO questions (question, answer, difficulty, category) 
                 VALUES ('Sport question', 'Sport answer', 5, 2);
@@ -61,7 +64,8 @@ class TriviaTestCase(unittest.TestCase):
             "category": 'Science',
         }
         try:
-            exists = next(q for q in result.json['questions'] if all(item in q.items() for item in question.items()))
+            exists = next(q for q in result.json['questions'] if
+                          all(item in q.items() for item in question.items()))
         except StopIteration:
             self.fail("Expected question was not found in the results")
         self.assertIsNotNone(exists)
@@ -79,15 +83,18 @@ class TriviaTestCase(unittest.TestCase):
         question_id = 100
         with self.app.app_context():
             value = self.db.engine.execute(f"""
-                INSERT INTO questions (id, question, answer, difficulty, category) 
+                INSERT INTO questions 
+                    (id, question, answer, difficulty, category) 
                 VALUES ({question_id}, 'Question 100', 'Answer 100', 3, 1)
             """)
             self.assertIsNotNone(value)
         result = client.delete(f"/api/questions/{question_id}")
         self.assertEqual(200, result.status_code)
-        self.assertDictEqual({"success": True, "deleted_question_id": question_id}, result.json)
+        self.assertDictEqual(
+            {"success": True, "deleted_question_id": question_id}, result.json)
         with self.app.app_context():
-            value = self.db.engine.execute(f"SELECT * FROM questions WHERE id={question_id}").first()
+            value = self.db.engine.execute(
+                f"SELECT * FROM questions WHERE id={question_id}").first()
             self.assertIsNone(value)
 
     def test_delete_question_fails_when_passed_nonexistent_id(self):
@@ -135,21 +142,27 @@ class TriviaTestCase(unittest.TestCase):
     def test_search_questions_returns_expected_results(self):
         client = self.client()
 
-        result1 = client.post("/api/search/questions", json={"searchTerm": "the"})
+        result1 = client.post("/api/search/questions",
+                              json={"searchTerm": "the"})
         self.assertEqual(200, result1.status_code)
         self.assertEqual(2, len(result1.json["questions"]))
 
-        result2 = client.post("/api/search/questions", json={"searchTerm": "bang"})
+        result2 = client.post("/api/search/questions",
+                              json={"searchTerm": "bang"})
         self.assertEqual(200, result2.status_code)
         self.assertEqual(1, len(result2.json["questions"]))
-        self.assertEqual("When did the big bang happen", result2.json["questions"][0]["question"])
+        self.assertEqual("When did the big bang happen",
+                         result2.json["questions"][0]["question"])
 
-        result3 = client.post("/api/search/questions", json={"searchTerm": "BANG"})
+        result3 = client.post("/api/search/questions",
+                              json={"searchTerm": "BANG"})
         self.assertEqual(200, result3.status_code)
         self.assertEqual(1, len(result3.json["questions"]))
-        self.assertEqual("When did the big bang happen", result3.json["questions"][0]["question"])
+        self.assertEqual("When did the big bang happen",
+                         result3.json["questions"][0]["question"])
 
-        result4 = client.post("/api/search/questions", json={"searchTerm": "NonexistentTerm"})
+        result4 = client.post("/api/search/questions",
+                              json={"searchTerm": "NonexistentTerm"})
         self.assertEqual(200, result4.status_code)
         self.assertEqual(0, len(result4.json["questions"]))
 
@@ -170,7 +183,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual('Science', result1.json['current_category'])
         self.assertEqual(2, result1.json['total_questions'])
         self.assertEqual(2, len(result1.json['questions']))
-        all(self.assertEqual('Science', question['category']) for question in result1.json['questions'])
+        all(self.assertEqual('Science', question['category']) for question in
+            result1.json['questions'])
 
         category_id_2 = 2
         result2 = client.get(f"/api/categories/{category_id_2}/questions")
@@ -179,20 +193,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(1, result2.json['total_questions'])
         self.assertEqual(1, len(result2.json['questions']))
 
-    def test_get_questions_by_category_fails_when_category_is_given_by_name(self):
+    def test_get_questions_by_category_fails_when_category_is_given_by_name(
+            self):
         client = self.client()
         category = 'Science'
         result = client.get(f"/api/categories/{category}/questions")
         self.assertEqual(404, result.status_code)
 
-    def test_get_questions_by_category_fails_when_passed_a_nonexistent_category_id(self):
+    def test_get_questions_by_category_fails_when_passed_a_nonexistent_category_id(
+            self):
         client = self.client()
         category_id = 1000
         result = client.get(f"/api/categories/{category_id}/questions")
         print(result.json)
         self.assertEqual(400, result.status_code)
 
-    def test_get_quiz_question_returns_a_question_in_the_specified_category(self):
+    def test_get_quiz_question_returns_a_question_in_the_specified_category(
+            self):
         client = self.client()
 
         for i in range(10):
